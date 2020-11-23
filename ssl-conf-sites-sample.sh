8bba2c330f24198d9f7dc8915c2ac7e91095b7e3
Ben Green <ben@bcgdesign.com>
2020-11-23 11:25:55 +0000

    Moving setup to separate functions

--- ssl-conf-sites-sample.sh
+++ ssl-conf-sites-sample.sh
@@ -10,7 +10,7 @@
 #
 # ALIASES is an associative array
 #   key:    (string) primary domain name - if it doesn't match one of the keys in DOMAINS, it will be ignored
-#   value:  (string array) alias domain names to be included in the SSL certificate
+#   value:  (string) alias domain names to be included in the SSL certificate, separated by a space
 #
 # These arrays will generate configuration files that will be stored in /ssl/conf
 # After generation they can be modified to suit your needs - after modification, the container should be restarted
@@ -20,4 +20,4 @@ DOMAINS["example.com"]="http://example"
 ALIASES["example.com"]="www.example.com" "ex.com" "www.ex.com"
 
 DOMAINS["test.com"]="http://test"
-ALIASES["test.com"]=(www.test.com"
+ALIASES["test.com"]="www.test.com"
