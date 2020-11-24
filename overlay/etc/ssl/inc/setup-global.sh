079c8861bdd4b107103bd02e3fe14671daa8ff20
Ben Green <ben@bcgdesign.com>
2020-11-24 14:39:56 +0000

    Requiring setup_global script

--- overlay/etc/ssl/inc/setup-global.sh
+++ overlay/etc/ssl/inc/setup-global.sh
@@ -7,7 +7,7 @@
 setup_global () {
 
     if [ ! -f ${SSL_GLOBAL_CFG} ] ; then
-        echo " - creating global configuration..."
+        echo " - creating global configuration file..."
         gomplate \
             -o ${SSL_GLOBAL_CFG} \
             -f ${TEMPLATES}/global.conf.tmpl
