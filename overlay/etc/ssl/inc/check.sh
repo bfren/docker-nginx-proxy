466799436f8ed0dd18c2877dac24740b05bc8c4e
Ben Green <ben@bcgdesign.com>
2021-02-07 14:36:33 +0000

    Reverting bash tests

--- overlay/etc/ssl/inc/check.sh
+++ overlay/etc/ssl/inc/check.sh
@@ -4,7 +4,10 @@
 # Ensure email is set
 #======================================================================================================================
 
-[[ -z "${LETS_ENCRYPT_EMAIL}" ]] && "LETS_ENCRYPT_EMAIL must be set before requesting SSL certificates."
+if [ -z "${LETS_ENCRYPT_EMAIL}" ] ; then
+    _error "LETS_ENCRYPT_EMAIL must be set before requesting SSL certificates."
+    exit 1
+fi
 
 
 #======================================================================================================================
@@ -12,7 +15,10 @@
 #======================================================================================================================
 
 SSL_CONF=${SSL}/conf.sh
-[[ ! -f ${SSL_CONF} ]] && _error "You must create ${SSL_CONF} - see ssl-conf-sample.sh."
+if [ ! -f ${SSL_CONF} ] ; then
+    _error "You must create ${SSL_CONF} - see ssl-conf-sample.sh."
+    exit 1
+fi
 
 declare -A DOMAINS
 declare -A ALIASES
@@ -25,7 +31,10 @@ source ${SSL_CONF}
 # Check whether or not domains have been registered
 #======================================================================================================================
 
-[[ "${#DOMAINS[@]}" = "0" ]] && _error "No domains have been registered for SSL."
+if [ "${#DOMAINS[@]}" = "0" ] ; then
+    _error "No domains have been registered for SSL."
+    exit 1
+fi
 
 
 #======================================================================================================================
