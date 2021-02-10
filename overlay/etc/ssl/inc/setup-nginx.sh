5b3588cac81ffba97c3627c8239b23e3c03faac7
Ben Green <ben@bcgdesign.com>
2021-02-10 18:36:58 +0000

    Tweaking config formatting

--- overlay/etc/ssl/inc/setup-nginx.sh
+++ overlay/etc/ssl/inc/setup-nginx.sh
@@ -11,11 +11,11 @@
 setup_nginx () {
 
     # give arguments friendly names
-    export IS_DEFAULT=${1}
-    export DOMAIN_NAME=${2}
-    export UPSTREAM=${3}
-    local DOMAIN_ALIASES=${4}
-    export DOMAIN_NGXCONF=${5}
+    export IS_DEFAULT="${1}"
+    export DOMAIN_NAME="${2}"
+    export UPSTREAM="${3}"
+    local DOMAIN_ALIASES="${4}"
+    export DOMAIN_NGXCONF="${5}"
 
     # paths to site configuration and custom config directory
     local SITE="${SITES}/${DOMAIN_NAME}"
