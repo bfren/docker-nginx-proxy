70b50f568769797998baab7788631a52a131a2f2
Ben Green <ben@bcgdesign.com>
2021-12-03 12:31:10 +0000

    Moving load conf to separate inc

---
+++ overlay/usr/lib/bf/inc/proxy-load-conf.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/with-contenv bash
+
+
+#======================================================================================================================
+# Create arrays and include configuration.
+#======================================================================================================================
+
+SSL_CONF=${PROXY_SSL}/conf.sh
+if [ ! -f ${SSL_CONF} ] ; then
+    bf-error "You must create ${SSL_CONF} - see ssl-conf-sample.sh."
+    exit 1
+fi
+
+declare -A DOMAINS
+declare -A ALIASES
+declare -A NGXCONF
+
+source ${SSL_CONF}
