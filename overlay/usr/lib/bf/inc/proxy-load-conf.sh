011c28869a5b4eab1d09597dc8152d7a740c232a
bfren <ben@bcgdesign.com>
2022-05-04 12:49:44 +0100

    Removing convert from setup

--- overlay/usr/lib/bf/inc/proxy-load-conf.sh
+++ overlay/usr/lib/bf/inc/proxy-load-conf.sh
@@ -7,17 +7,8 @@
 
 SSL_CONF=${PROXY_SSL}/conf.json
 if [ ! -f ${SSL_CONF} ] ; then
-
-    # if a <=v3 bash configuration file doesn't exist either, exit with an error
-    OLD_SSL_CONF=${PROXY_SSL}/conf.sh
-    if [ ! -f ${OLD_SSL_CONF} ] ; then
-        bf-error "You must create ${SSL_CONF} - see ssl-conf-sample.json." "inc/proxy-load-conf.sh"
-        exit 1
-    fi
-
-    # attempt to create the JSON configuration file from a <=v3 bash configuration file
-    ${PROXY_LIB}/convert "${OLD_SSL_CONF}" "${SSL_CONF}"
-
+    bf-error "You must create ${SSL_CONF} - see ssl-conf-sample.json." "inc/proxy-load-conf.sh"
+    exit 1
 fi
 
 
