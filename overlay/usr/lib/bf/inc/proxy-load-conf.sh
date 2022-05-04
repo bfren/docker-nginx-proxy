2d31da3bae24b58b9a0ecd24b6adc8c7bc5b55ce
bfren <ben@bcgdesign.com>
2022-05-04 13:29:54 +0100

    Using helper functions to get config info

--- overlay/usr/lib/bf/inc/proxy-load-conf.sh
+++ overlay/usr/lib/bf/inc/proxy-load-conf.sh
@@ -29,3 +29,7 @@ declare -a DOMAINS=(`jq -r '.domains[].primary' <<< "${JSON}"`)
 #======================================================================================================================
 
 function get-domain() { jq --arg PRIMARY "${1}" '.domains[] | select(.primary == $PRIMARY)' <<< "${JSON}" ; }
+
+function get-upstream() { get-domain "${1}" | jq -r '.upstream' ; }
+function get-aliases() { get-domain "${1}" | jq -r '.aliases[]?' ; }
+function get-custom() { get-domain "${1}" | jq -r '.custom == true' ; }
