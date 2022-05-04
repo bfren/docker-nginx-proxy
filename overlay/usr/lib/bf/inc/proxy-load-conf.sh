40a059fcef9a09e7d18cb6b6a69ef7102f9a65bf
bfren <ben@bcgdesign.com>
2022-05-04 12:10:15 +0100

    Load domains config from JSON not bash

--- overlay/usr/lib/bf/inc/proxy-load-conf.sh
+++ overlay/usr/lib/bf/inc/proxy-load-conf.sh
@@ -2,17 +2,29 @@
 
 
 #======================================================================================================================
-# Create arrays and include configuration.
+# Check JSON configuration file exists.
 #======================================================================================================================
 
-SSL_CONF=${PROXY_SSL}/conf.sh
+SSL_CONF=${PROXY_SSL}/conf.json
 if [ ! -f ${SSL_CONF} ] ; then
-    bf-error "You must create ${SSL_CONF} - see ssl-conf-sample.sh." "inc/proxy-load-conf.sh"
-    exit 1
+
+    # if a <=v3 bash configuration file doesn't exist either, exit with an error
+    OLD_SSL_CONF=${PROXY_SSL}/conf.sh
+    if [ ! -f ${OLD_SSL_CONF} ] ; then
+        bf-error "You must create ${SSL_CONF} - see ssl-conf-sample.json." "inc/proxy-load-conf.sh"
+        exit 1
+    fi
+
+    # attempt to create the JSON configuration file from a <=v3 bash configuration file
+    ${PROXY_LIB}/convert "${OLD_SSL_CONF}" "${SSL_CONF}"
+
 fi
 
-declare -A DOMAINS
-declare -A ALIASES
-declare -A NGXCONF
 
-source ${SSL_CONF}
+#======================================================================================================================
+# Load JSON and create DOMAINS array by selecting primary keys.
+#======================================================================================================================
+
+CONF_JSON=`cat "${SSL_CONF}" | jq '.'`
+
+declare -a DOMAINS=(`jq -r '.domains[].primary' <<< "${CONF_JSON}"`)
