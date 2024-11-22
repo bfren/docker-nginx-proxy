ff75b9afe5c767952e43351f3eaa2459494f6cae
bfren <bf@bfren.dev>
2024-11-22 10:49:24 +0000

    Updating test.sh

--- test.sh
+++ test.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 
-IMAGE=`cat VERSION`
+IMAGE=nginx-proxy
+VERSION=`cat VERSION`
+ALPINE=${1:-3.20}
+TAG=${IMAGE}-test
 
 docker buildx build \
     --load \
-    --build-arg BF_IMAGE=nginx-proxy \
-    --build-arg BF_VERSION=${IMAGE} \
+    --build-arg BF_IMAGE=${IMAGE} \
+    --build-arg BF_VERSION=${VERSION} \
     -f Dockerfile \
-    -t nginx-proxy-test \
+    -t ${TAG} \
     . \
     && \
-    docker run --entrypoint "/usr/bin/env" nginx-proxy-test env -i nu -c "use bf test ; test"
+    docker run --env-file ./test.env --entrypoint /test ${TAG}
